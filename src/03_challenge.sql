ea9cc9a7d73692c663b8e273ea9ca60ae3341330
Thercístenes <thercistenes1@outlook.com>
2024-01-17 08:45:45 -0300

    03. Escreva uma query que cria a tabela users

---
+++ src/03_challenge.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users(
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL,
+  email VARCHAR(60),
+  age INTEGER,
+  created_at DATE NOT NULL DEFAULT(NOW())
+);
